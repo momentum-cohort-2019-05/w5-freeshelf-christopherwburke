fd5d9502f0ef8caa4fe72f20dfee353fa9063bee
christopherwburke <christopher.w.burke@gmail.com>
2019-06-28 11:52:31 -0400

    About to run make migrations for the first time

--- core/models.py
+++ core/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Book(models.model):
+    title = models.Charfield(max_length=200)
+    author = models.Charfield(max_length=100)
+    description = models.Charfield(max_length=1000)
+    URL = models.URLField(unique=True)
+    slug = models.Slugfield(max_length=100)
+    date = models.DateTimeField(default=date.today)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.model):
+    title = models.Charfield(max_length=200)
+    slug = models.Slugfield(max_length=100)
+
+    def __str__(self):
+        return self.title
 
