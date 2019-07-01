d3d659648c94622802dc7e6c3d82e438ccb02da5
christopherwburke <christopher.w.burke@gmail.com>
2019-07-01 09:54:26 -0400

    Commit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Book, Category
 
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Category)
--- core/models.py
+++ core/models.py
@@ -3,20 +3,20 @@ from datetime import date
 
 # Create your models here.
 
-class Book(models.model):
-    title = models.Charfield(max_length=200)
-    author = models.Charfield(max_length=100)
-    description = models.Charfield(max_length=1000)
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
     URL = models.URLField(unique=True)
-    slug = models.Slugfield(max_length=100)
-    date = models.DateTimeField(default=date.today)
+    slug = models.SlugField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
-class Category(models.model):
-    title = models.Charfield(max_length=200)
-    slug = models.Slugfield(max_length=100)
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100)
 
     def __str__(self):
         return self.title
